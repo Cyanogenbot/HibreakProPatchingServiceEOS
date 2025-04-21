49b6e1c7ea89b5a30adcd20a6706d5ffa1d1b116
Vasu Bhatia <git@vbh.ai>
2025-04-21 19:42:53 +0100

    Update Android.mk to to replace CPP flags with C flags

--- app/external_scripts/root_service/jni/Android.mk
+++ app/external_scripts/root_service/jni/Android.mk
@@ -1,10 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
 
+include $(CLEAR_VARS)
 LOCAL_MODULE := a9_eink_server
 LOCAL_SRC_FILES := eink_daemon.c
-LOCAL_CPPFLAGS := -std=gnu++0x -Wall -Wextra -Werror -fPIE -D_FORTIFY_SOURCE=2
+LOCAL_CFLAGS := -Wall -Wextra -Werror -fPIE -D_FORTIFY_SOURCE=2
 LOCAL_LDLIBS := -llog
-
 include $(BUILD_EXECUTABLE)
