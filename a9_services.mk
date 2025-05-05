4a0fbb637dcf192b0d84a649cbd4d80358e15129
Vasu Bhatia <git@vbh.ai>
2025-05-05 17:11:51 +0000

    Update Makefile

--- a9_services.mk
+++ a9_services.mk
@@ -4,3 +4,5 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.accessibility.enabled_service=com.lmqr.ha9_comp_service/.A9AccessibilityService
+
+BOARD_VENDOR_SEPOLICY_DIRS += vendor/a9_services/sepolicy/public
