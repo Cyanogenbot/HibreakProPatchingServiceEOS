122f5c96d0827e172ed4db67ef44ab313f58675a
Vasu Bhatia <git@vbh.ai>
2025-05-05 09:35:17 +0000

    Remove duplicate .rc rules

--- a9_services.mk
+++ a9_services.mk
@@ -4,6 +4,3 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.accessibility.enabled_service=com.lmqr.ha9_comp_service/.A9AccessibilityService
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/a9_eink_daemon/etc/init/a9_eink_daemon.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/a9_eink_daemon.rc
